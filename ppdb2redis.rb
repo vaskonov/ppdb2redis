51de6921a65a909340fb541be499afd326e645de
vasily <elgesto@gmail.com>
2015-06-06 06:10:29 +0300

    Hardcoded filename

--- ppdb2redis.rb
+++ ppdb2redis.rb
@@ -31,7 +31,7 @@ count = 0
 filename = ARGV[1]
 filename = ARGV[2]
 
-file = File.new("", "r")
+file = File.new("ppdb-1.0-xl-all", "r")
 #file = File.new("ppdb", "r")
 while (line = file.gets)
     parammain = {}
